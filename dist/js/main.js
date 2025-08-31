edad1f9ccd0a425f51fd5a3cc72d4668f08ff031
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-08-31 19:09:49 +0100

    continued adding functionality to the buttons

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,9 @@
-import { addSpinner } from "./domFunctions.js";
+import {
+  addSpinner,
+  displayError,
+  updateScreenReaderConfirmation,
+} from "./domFunctions.js";
+import { setLocationObject, getHomeLocation } from "./dataFunctions.js";
 import CurrentLocation from "./CurrentLocation.js";
 
 const currentLoc = new CurrentLocation();
@@ -7,8 +12,13 @@ const initApp = () => {
   // add listeners
   const geoButton = document.getElementById("getLocation");
   geoButton.addEventListener("click", getGeoWeather);
+  const homeButton = document.getElementById("home");
+  homeButton.addEventListener("click", loadWeather);
+  const saveButton = document.getElementById("saveLocation");
+  saveButton.addEventListener("click", saveLocation);
   // set up
   // load weather
+  loadWeather();
 };
 
 document.addEventListener("DOMContentLoaded", initApp);
@@ -16,10 +26,79 @@ document.addEventListener("DOMContentLoaded", initApp);
 const getGeoWeather = (event) => {
   if (event) {
     if (event.type === "click") {
-      const mapIcon = document.getElementById(".fa-map-marker-alt");
+      const mapIcon = document.querySelector(".fa-map-marker-alt");
       addSpinner(mapIcon);
     }
   }
-  //   if (!navigator.geolocation) return geoError();
-  //   navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+  if (!navigator.geolocation) return geoError();
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+};
+
+const geoError = (errObj) => {
+  const errMsg = errObj ? errObj.message : "Geolocation not supported";
+  displayError(errMsg, errMsg);
+};
+
+const geoSuccess = (position) => {
+  const myCoordsObj = {
+    lat: position.coords.latitude,
+    lon: position.coords.longitude,
+    name: `Lat: ${position.coords.latitude} Long: ${position.coords.longitude}`,
+  };
+  setLocationObject(currentLoc, myCoordsObj);
+  updateDataAndDisplay(currentLoc);
+};
+
+const loadWeather = (event) => {
+  const savedLocation = getHomeLocation();
+  if (!savedLocation && !event) return getGeoWeather();
+  if (!savedLocation && event.type === "click") {
+    displayError(
+      "No Home Location Saved.",
+      "Sorry, Please save home location first."
+    );
+  } else if (savedLocation && !event) {
+    displayHomeLocationWeather(savedLocation);
+  } else {
+    const homeIcon = document.querySelector(".fa-home");
+    addSpinner(homeIcon);
+    displayHomeLocationWeather(savedLocation);
+  }
+};
+
+const displayHomeLocationWeather = (home) => {
+  if (typeof home === "string") {
+    const locationJSON = JSON.parse(home);
+    const myCoordsObj = {
+      lat: locationJSON.lat,
+      lon: locationJSON.lon,
+      name: locationJSON.name,
+      unit: locationJSON.unit,
+    };
+    setLocationObject(currentLoc, myCoordsObj);
+    updateDataAndDisplay(currentLoc);
+  }
+};
+
+const saveLocation = () => {
+  if (currentLoc.getLat() && currentLoc.getLon()) {
+    const saveIcon = document.querySelector(".fa-save");
+    addSpinner(saveIcon);
+    const location = {
+      name: currentLoc.getName(),
+      lat: currentLoc.getLat(),
+      lon: currentLoc.getLon(),
+      unit: currentLoc.getUnit(),
+    };
+    localStorage.setItem("defaultWeatherLocation", JSON.stringify(location));
+    updateScreenReaderConfirmation(
+      `Saved ${currentLoc.getName()} as home location.`
+    );
+  }
+};
+
+const updateDataAndDisplay = async (locationObj) => {
+  console.log(locationObj);
+  // const weatherJson = await getWeatherFromCoords(locationObj);
+  // if (weatherJson) updateDisplay(weatherJson, locationObj);
 };
