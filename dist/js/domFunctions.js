93cca2de53ee9ca31f099c2cd8e4b07b3f4d32cb
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-09-06 13:54:28 +0100

    continued building the update display function

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -56,7 +56,12 @@ export const updateDisplay = (weatherJson, locationObj) => {
   updateScreenReaderConfirmation(screenReaderWeather);
   updateWeatherLocationHeader(locationObj.getName());
   //current conditions
+  const ccArray = createCurrentConditionsDivs(
+    weatherJson,
+    locationObj.getUnit()
+  );
   //six day forecast
+
   setFocusOnSearch();
   fadeDisplay();
 };
@@ -126,4 +131,122 @@ const buildScreenReaderWeather = (weatherJson, locationObj) => {
 
 const setFocusOnSearch = () => {
   document.getElementById("searchBar__text").focus();
+};
+
+const createCurrentConditionsDivs = (weatherObj, unit) => {
+  const tempUnit = unit === "imperial" ? "F" : "C";
+  const windUnit = unit === "imperial" ? "mph" : "m/s";
+  const icon = createMainImgDiv(
+    weatherObj.current.weather[0].icon,
+    weatherObj.current.weather[0].description
+  );
+  const temp = createElem(
+    "div",
+    "temp",
+    `${Math.round(Number(weatherObj.current.temp))}°`
+  );
+  const properDesc = toProperCase(weatherObj.current.weather[0].description);
+  const desc = createElem("div", "desc", properDesc);
+  const feels = createElem(
+    "div",
+    "feels",
+    `feels like ${Math.round(Number(weatherObj.current.feels_like))}°`
+  );
+  const maxTemp = createElem(
+    "div",
+    "maxTemp",
+    `High ${Math.round(Number(weatherObj.daily[0].temp.max))}°`
+  );
+  const minTemp = createElem(
+    "div",
+    "minTemp",
+    `Low ${Math.round(Number(weatherObj.daily[0].temp.min))}°`
+  );
+  const humidity = createElem(
+    "div",
+    "humidity",
+    `Humidity ${weatherObj.current.humidity}%`
+  );
+  const wind = createElem(
+    "div",
+    "wind",
+    `Wind ${Math.round(Number(weatherObj.current.wind_speed))} ${windUnit}`
+  );
+
+  return [icon, temp, desc, feels, maxTemp, minTemp, humidity, wind];
+};
+
+const createMainImgDiv = (icon, altText) => {
+  const iconDiv = createElem("div", "icon");
+  iconDiv.id = "icon";
+  const faIcon = translateIconToFontAwesome(icon);
+  faIcon.ariaHidden = true;
+  faIcon.title = altText;
+  iconDiv.appendChild(faIcon);
+  return iconDiv;
+};
+
+const createElem = (elemType, divClassName, divText, unit) => {
+  const div = document.createElement(elemType);
+  div.className = divClassName;
+  if (divText) {
+    div.textContent = divText;
+  }
+  if (divClassName === "temp") {
+    const unitDiv = document.createElement("div");
+    unitDiv.classList.add("unit");
+    unitDiv.textContent = unit;
+    div.appendChild(unitDiv);
+  }
+  return div;
+};
+
+const translateIconToFontAwesome = () => {
+  const i = document.createElement("i");
+  const firstTwoChars = icon.slice(0, 2);
+  const lastChar = icon.slice(2);
+  switch (firstTwoChars) {
+    case "01":
+      if (lastChar === "d") {
+        i.classList.add("far", "fa-sun");
+      } else {
+        i.classList.add("far", "fa-moon");
+      }
+      break;
+    case "02":
+      if (lastChar === "d") {
+        i.classList.add("fas", "fa-cloud-sun");
+      } else {
+        i.classList.add("fas", "fa-cloud-moon");
+      }
+      break;
+    case "03":
+      i.classList.add("fas", "fa-cloud");
+      break;
+    case "04":
+      i.classList.add("fas", "fa-cloud-meatball");
+      break;
+    case "09":
+      i.classList.add("fas", "fa-cloud-rain");
+      break;
+    case "10":
+      if (lastChar === "d") {
+        i.classList.add("fas", "fa-cloud-sun-rain");
+      } else {
+        i.classList.add("fas", "fa-cloud-moon-rain");
+      }
+      break;
+    case "11":
+      i.classList.add("fas", "fa-poo-storm");
+      break;
+    case "13":
+      i.classList.add("far", "fa-snowflake");
+      break;
+    case "50":
+      i.classList.add("fas", "fa-smog");
+      break;
+    default:
+      i.classList.add("far", "fa-question-circle");
+  }
+  return i;
 };
