0e614182e0674dc1803ed8720e1afc6280e2ce1a
Oladapo Oluwadurotimi <oladapooluwadurotimi@gmail.com>
2025-09-08 06:38:09 +0100

    removed environment variable from local environment

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,5 +1,3 @@
-import { WEATHER_API_KEY } from "./config.js";
-
 export const setLocationObject = (locationObj, coordsObj) => {
   const { lat, lon, name, unit } = coordsObj;
   locationObj.setLat(lat);
